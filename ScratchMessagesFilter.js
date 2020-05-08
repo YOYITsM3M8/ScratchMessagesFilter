157ae3396b5efed03e311cd6aea194825ee52410
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-05-08 12:46:43 -0400

    Update ScratchMessagesFilter.js

--- ScratchMessagesFilter.js
+++ ScratchMessagesFilter.js
@@ -10,17 +10,7 @@
 
 window.onload = (event) => {
     var script = document.createElement('script');
-    script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    script.src = 'https://raw.githubusercontent.com/YOYITsM3M8/ScratchMessagesFilter/master/scrpt.js';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
-    dele();
 };
-
-function dele() {
-    var forum_activity = $(".mod-forum-activity");
-    var i;
-    for (i = 0; i < forum_activity.length; i++) {
-        forum_activity[i].remove();
-    }
-    setInterval(function(){ dele(); }, 500);
-}
