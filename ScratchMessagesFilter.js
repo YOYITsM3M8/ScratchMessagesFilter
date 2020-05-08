0bdf8c476cb30f49e1f8237a77b45b9b0b204ae5
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-05-08 12:49:14 -0400

    Update ScratchMessagesFilter.js

--- ScratchMessagesFilter.js
+++ ScratchMessagesFilter.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       Scratch Messages Filter
-// @namespace  scratch.mit.edu/users/YOYITsM3M8
+// @namespace  https://scratch.mit.edu/users/YOYITsM3M8
 // @version    1.0
 // @description Hides messages you don't care about and shows the ones you do.
 // @match      https://scratch.mit.edu/messages/*
@@ -10,7 +10,7 @@
 
 window.onload = (event) => {
     var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/YOYITsM3M8/ScratchMessagesFilter/master/scrpt.js';
+    script.src = 'https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/YOYITsM3M8/ScratchMessagesFilter/master/scrpt.js';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 };
